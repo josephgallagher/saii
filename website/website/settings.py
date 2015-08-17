c1c5a84913b6cebda2e777d21f327a896bfae927
Joseph Gallagher <uhhhmmm@gmail.com>
2015-08-17 10:31:39 -0400

    mail server running

--- website/website/settings.py
+++ website/website/settings.py
@@ -87,7 +87,14 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 )
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+OSCAR_FROM_EMAIL = 'joseph@josephdgallagher.com'
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.zoho.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = OSCAR_FROM_EMAIL
+EMAIL_HOST_PASSWORD = "joseph#^#"
 
 AUTHENTICATION_BACKENDS = (
     'oscar.apps.customer.auth_backends.EmailBackend',
