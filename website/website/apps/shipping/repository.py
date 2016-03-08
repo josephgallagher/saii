88f038627e961532b3a72d9c9d7e401943a6df9a
Joseph Gallagher <uhhhmmm@gmail.com>
2016-03-08 06:29:09 -0500

    Removed shipping references...

--- website/website/apps/shipping/repository.py
+++ website/website/apps/shipping/repository.py
@@ -3,17 +3,17 @@ from oscar.apps.shipping import repository
 from . import methods as shipping_methods
 
 
-class Repository(repository.Repository):
-    # methods = (methods.UPS(), methods.FedExSecond(), methods.FedExNext(), methods.FedExInternational())
-
-    def get_available_shipping_methods(self, basket, user=None, shipping_addr=None, request=None, **kwargs):
-        if shipping_addr and shipping_addr.country.code == 'US':
-            methods = (shipping_methods.UPS(), shipping_methods.FedExSecond(), shipping_methods.FedExNext())
-            return methods
-        elif shipping_addr and shipping_addr.country.code == 'CA':
-            methods = (shipping_methods.UPS(), shipping_methods.FedExSecond(), shipping_methods.FedExNext(),
-                       shipping_methods.FedExInternational())
-            return methods
-        else:
-            methods = (shipping_methods.FedExInternational(),)
-            return methods
+# class Repository(repository.Repository):
+#     # methods = (methods.UPS(), methods.FedExSecond(), methods.FedExNext(), methods.FedExInternational())
+#
+#     def get_available_shipping_methods(self, basket, user=None, shipping_addr=None, request=None, **kwargs):
+#         if shipping_addr and shipping_addr.country.code == 'US':
+#             methods = (shipping_methods.UPS(), shipping_methods.FedExSecond(), shipping_methods.FedExNext())
+#             return methods
+#         elif shipping_addr and shipping_addr.country.code == 'CA':
+#             methods = (shipping_methods.UPS(), shipping_methods.FedExSecond(), shipping_methods.FedExNext(),
+#                        shipping_methods.FedExInternational())
+#             return methods
+#         else:
+#             methods = (shipping_methods.FedExInternational(),)
+#             return methods
